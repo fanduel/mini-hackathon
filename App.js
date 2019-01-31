58946037698e746fae76eca21fe856ddcfa152d6
Robert Hewitt <fuzzybear@gmail.com>
2019-01-31 15:42:02 +0000

    updated json url

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-const playerDataUrl = "https://gist.github.com/liamjdouglas/bb40ee8721f1a9313c22c6ea0851a105"
+const playerDataUrl = "https://gist.githubusercontent.com/liamjdouglas/bb40ee8721f1a9313c22c6ea0851a105/raw/6b6fc89d55ebe4d9b05c1469349af33651d7e7f1/Player.json"
 
 export default class App extends React.Component {
   render() {
